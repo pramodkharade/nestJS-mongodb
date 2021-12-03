80d94174e5ccf842a0623a6b4eae4e5568b4ab51
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2021-12-03 18:13:45 +0530

    install momngoose and their module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule,
+  MongooseModule.forRoot('mongodb://localhost:27017/nestJS-MongoDB')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ProductSchema } from './Product.model';
 @Module({
   providers: [ProductsService],
   controllers: [ProductsController],
+  imports:[MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),]
 })
 export class ProductsModule {}
